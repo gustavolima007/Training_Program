a7ded2ed1aea6616b292ae14615d2ae7cf667fc7
gustavolima007 <adm.gustavo.lima@gmail.com>
2021-04-10 18:55:13 -0300

    SQl queries

---
+++ SQL and Databases/POSTGRESQL/Query/Select and comment.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2141014c823d3ef09a1db554e8a1ae677e2e656e8665385af829d69e63ef417
+size 269
--- SQL and Databases/POSTGRESQL/commands.sql
+++ SQL and Databases/POSTGRESQL/commands.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e7db5a0d3a5405e37faac423b4ecceff1549fee20531a54b7c7ad9244f46a96
-size 383
+oid sha256:beda6bbbf95fcbf45c1e33bbedb45ee12f0450e38c77a472f68e473d8ae2bf6b
+size 492
